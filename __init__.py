0838e7b8c6a8b95912e546fd42fb47e683d49f4a
Alvaro <alvaro347@gmail.com>
2018-02-19 18:43:25 +0000

    Added Ouath lib

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ from flask import make_response
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Gallery, Pictures, User
-from oauth2client.client import flow_from_clientsecrets
+from lib.oauth2client.client import flow_from_clientsecrets
 from lib.oauth2client.client import FlowExchangeError
 from werkzeug.utils import secure_filename
 
