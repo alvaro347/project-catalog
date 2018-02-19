a5a08e78c5c21374e6d7ade6882fbb5b70485369
Alvaro <alvaro347@gmail.com>
2018-02-19 22:36:01 +0000

    test

--- main/__init__.py
+++ main/__init__.py
@@ -9,19 +9,19 @@ from flask import redirect, jsonify, url_for, flash
 from flask import session as login_session
 from flask import make_response
 
-from sqlalchemy import create_engine, asc
-from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Gallery, Pictures, User
-from oauth2client.client import flow_from_clientsecrets
-from oauth2client.client import FlowExchangeError
-from werkzeug.utils import secure_filename
-
-import httplib2
-import requests
-import random
-import string
-import json
-import os
+# from sqlalchemy import create_engine, asc
+# from sqlalchemy.orm import sessionmaker
+# from database_setup import Base, Gallery, Pictures, User
+# from oauth2client.client import flow_from_clientsecrets
+# from oauth2client.client import FlowExchangeError
+# from werkzeug.utils import secure_filename
+#
+# import httplib2
+# import requests
+# import random
+# import string
+# import json
+# import os
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'static'
