ee2fecf74b9ecb80cb5532d8eaa9ddd8cab64a59
Alvaro <alvaro347@gmail.com>
2018-02-19 19:39:40 +0000

    added host 127.0.0.1

--- __init__.py
+++ __init__.py
@@ -425,4 +425,4 @@ def galleriesJSON():
 if __name__ == '__main__':
     app.secret_key = 'key'
     app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='127.0.0.1', port=5000)
