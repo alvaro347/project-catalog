05ab90e2805405b4090e59d94459eeb07b62094d
Alvaro <alvaro347@gmail.com>
2018-02-20 08:26:15 +0000

    uncomment 2

--- main/__init__.py
+++ main/__init__.py
@@ -5,25 +5,25 @@
 # apis, upload files, etc.
 
 from flask import Flask, render_template, request
-# from flask import redirect, jsonify, url_for, flash
-# from flask import session as login_session
-# from flask import make_response
-# from sqlalchemy import create_engine, asc
-# from sqlalchemy.orm import sessionmaker
-# from database_setup import Base, Gallery, Pictures, User
-# from oauth2client.client import flow_from_clientsecrets
-# from oauth2client.client import FlowExchangeError
-# from werkzeug.utils import secure_filename
-# import httplib2
-# import requests
-# import random
-# import string
-# import json
-# import os
+from flask import redirect, jsonify, url_for, flash
+from flask import session as login_session
+from flask import make_response
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Gallery, Pictures, User
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+from werkzeug.utils import secure_filename
+import httplib2
+import requests
+import random
+import string
+import json
+import os
 
 app = Flask(__name__)
-# UPLOAD_FOLDER = 'static'
-# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+UPLOAD_FOLDER = 'static'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # NOTE: Connect to Database and create database session
 
